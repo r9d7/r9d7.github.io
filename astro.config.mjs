c82353de4a5a10d74020747676e2c12a6f8698e9
duckradu <radu.dascalu@pm.me>
2024-08-05 20:39:02 +0100

    Idk what's going on

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import solidJs from "@astrojs/solid-js";
+import UnoCss from "unocss/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://whaaaaaaaastro.com",
+  integrations: [solidJs(), UnoCss({ injectReset: true })],
+});
