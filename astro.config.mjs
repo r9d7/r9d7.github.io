f33385faaaf5c11212d3629e376e68f7cd1df388
duckradu <radu.dascalu@pm.me>
2024-08-05 21:56:15 +0100

    Add remarkReadingTime plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import UnoCss from "unocss/astro";
 
+import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://whaaaaaaaastro.com",
   integrations: [solidJs(), UnoCss({ injectReset: true })],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
