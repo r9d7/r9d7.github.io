8f98bfd77afcc4c67c143b4e499a8f5905ea7a8a
Radu Dascalu <radu.dascalu@pm.me>
2024-08-28 20:48:50 +0100

    Add post table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "astro/config";
-import solidJs from "@astrojs/solid-js";
-import UnoCss from "unocss/astro";
-import remarkToc from "remark-toc";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import solidJs from "@astrojs/solid-js";
+import { defineConfig } from "astro/config";
+import remarkCollapse from 'remark-collapse';
+import remarkToc from "remark-toc";
+import UnoCss from "unocss/astro";
 
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
@@ -13,7 +14,8 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [
       remarkReadingTime,
-      [remarkToc, { heading: "toc", maxDepth: 3 }],
+      [remarkToc, { maxDepth: 3 }],
+      [remarkCollapse, { test: 'toc', summary: "Table of Contents" }],
     ],
     rehypePlugins: [rehypeHeadingIds],
   },
