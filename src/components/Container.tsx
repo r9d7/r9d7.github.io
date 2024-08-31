ff5bc5593df316900c496cea37c4b18b56f6d8f2
Radu Dascalu <radu.dascalu@pm.me>
2024-08-31 10:19:55 +0100

    Pass `class` prop

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,15 @@
 import type { ParentProps } from "solid-js";
 
-export type ContainerProps = ParentProps;
+export type ContainerProps = ParentProps<{ class?: string }>;
 
 export function Container(props: ContainerProps) {
   return (
-    <div class="w-full max-w-screen-md mx-auto px-4">{props.children}</div>
+    <div
+      class={["w-full max-w-screen-sm mx-auto px-4", props.class]
+        .filter(Boolean)
+        .join(" ")}
+    >
+      {props.children}
+    </div>
   );
 }
